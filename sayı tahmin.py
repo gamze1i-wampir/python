83b170b1c005a7d5e850faa1598dd80fd688cb0f
gamze1i-wamp!r <50878750+gamze1i-wampir@users.noreply.github.com>
2019-09-22 21:32:30 +0300

    python sayı tahmin oyunu
    pythonda sayı tahmin oyunu

---
+++ sayı tahmin.py
@@ -0,0 +1,45 @@
+print("********** TURKHACKTEAM")
+print("\tTHT Gamze1i-wamp!r")
+print(" Sayı tahmin oyunu**********\n")
+
+tahminisayi = "56" 
+
+yanlış_tahmin = 0
+while True: 
+    tahmini_sayi_giriş = input("tahmini sayıyı bul :")
+   
+
+    if (tahmini_sayi_giriş != tahminisayi ):
+        print("sayıyı yanlış tahmin etiniz ...")
+        yanlış_tahmin += 1
+        print("yanlış tahmin sayısı : ", yanlış_tahmin)
+    elif (tahmini_sayi_giriş == tahminisayi ):
+        print("tahmini doğru buldunuz")
+
+
+        break
+        print("********** TURKHACKTEAM")
+print("\tTHT Gamze1i-wamp!r")
+print(" Sayı tahmin oyunu 2   **********\n")
+
+tahminisayi = "76" 
+
+yanlış_tahmin = 0
+while True: 
+    tahmini_sayi_giriş = input("tahmini sayıyı bul :")
+   
+
+    if (tahmini_sayi_giriş != tahminisayi ):
+        print("sayıyı yanlış tahmin etiniz ...")
+        yanlış_tahmin += 1
+        print("yanlış tahmin sayısı : ", yanlış_tahmin)
+    elif (tahmini_sayi_giriş == tahminisayi ):
+        print("tahmini doğru buldunuz")
+        print("TEBRİKLER BÜTÜN BÖLÜMLERİ BİTİRDİNİZ")
+        print("PYTHON SAYI TAHMİN OYUNU ")
+        print("YAPIMCI : Gamze1i-wamp!r THT  ")
+        print("oyun gelişme aşamasındadır..........")
+        
+
+
+  
